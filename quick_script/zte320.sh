102fe913dcc41b3d214a1edee65a86840bbe742d
ra.andreev <ra.andreev@rk.t72.ru>
2026-05-01 18:39:26 +0500

    add quick_script for zte320

---
+++ quick_script/zte320.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/expect -f
+# Использовать: ./ltp.exp <HOST> <USER_PORT>
+
+# Задаем таймаут ожидания (в секундах)
+set timeout 10
+
+# Проверяем количество аргументов
+if {$argc < 2} {
+    puts "Usage: $argv0 <HOST> <USER_PORT>"
+    exit 1
+}
+
+set HOST [lindex $argv 0]
+set USER_PORT "gpon-onu_[lindex $argv 1]"
+set DELAY 1
+
+# Если нужно, можно захардкодить: set USER "admin"
+set USER $env(USER)
+set PASS $env(PASS)
+
+spawn telnet $HOST
+
+expect {
+    "Username:" { send "$USER\r"; exp_continue }
+    "Password:" { send "$PASS\r" }
+    timeout { puts "Таймаут при ожидании запроса пароля"; exit 1 }
+}
+
+expect -re {[#>]$}
+
+# Список команд
+set commands [list \
+    "show gpon onu detail-info $USER_PORT" \
+    " " \
+    "show pon power attenuation $USER_PORT" \
+    "show mac-real-time gpon onu $USER_PORT" \
+    "show gpon remote-onu interface eth $USER_PORT" \
+    "show gpon remote-onu interface video-ani $USER_PORT" \
+]
+
+# Выполняем команды
+foreach cmd $commands {
+    send "$cmd\r"
+    expect -re {[#>]$}
+    sleep $DELAY
+}
+
+# Передаем управление пользователю (остаемся в интерфейсе коммутатора)
+interact
